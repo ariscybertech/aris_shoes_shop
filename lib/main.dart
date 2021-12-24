52126fdc9e3318b415f11e937534ca604555f558
ariscybertech <ariscybertech@gmail.com>
2021-12-24 10:43:27 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shoes_shop/aris/pages/root_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RootPage(),
+    ),
+  );
+}
